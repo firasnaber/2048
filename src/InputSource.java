3a67e736e9f1b3cfeb365eaa1c9f5c85e57624dc
Firas Naber <firasnaber@gmail.com>
2025-06-16 19:18:16 -0500

    Add game code files

---
+++ src/InputSource.java
@@ -0,0 +1,16 @@
+package src;
+
+/** Describes a source of input commands.
+ *  @author P. N. Hilfinger
+ */
+interface InputSource {
+
+    /** Returns one command string. */
+    String getKey();
+
+    /** Returns a candidate Tile whose row and column is in the range
+     *  0 .. SIZE-1.  */
+    Tile getNewTile(int size);
+
+}
+
